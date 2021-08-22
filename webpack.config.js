fcc8901d027fd8f4ee031f848940c880db9e7a3d
wy <512458266@qq.com>
2021-08-22 13:24:36 +0800

    ainit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/gitpost.js',
     output: {
         filename: 'gitpost.js',
         path: path.resolve(__dirname, 'public'),
