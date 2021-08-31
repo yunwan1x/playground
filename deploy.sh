b00832f596f1384fa554687c3e1cd2deb67acb30
yun wang <512458266@qq.com>
2021-09-01 00:32:24 +0800

    ainit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-cp /mnt/d/github/webpack-demo/src/gitpost.js /mnt/d/github/yunwan1x.github.io/assets/vendor/gitpost
-cp   /mnt/d/github/webpack-demo/src/gitpost.js   /mnt/d/github/my-document/docs/js/
+cp /mnt/d/github/gitpod/src/gitpost.js /mnt/d/github/yunwan1x.github.io/assets/vendor/gitpost
+cp   /mnt/d/github/gitpod/src/gitpost.js   /mnt/d/github/my-document/docs/js/
